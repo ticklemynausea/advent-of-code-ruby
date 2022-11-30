5ab9c5699e0e01b68872cda6fa22244f4357ec61
Mário Carneiro <emailoftheyear@gmail.com>
2022-11-30 16:32:34 +0000

    Add boilerplate

---
+++ lib/advent2022.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "advent2022/version"
+require_relative "advent2022/day01"
+
+module Advent2022
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/advent2022/day01/day01.rb
@@ -0,0 +1,9 @@
+module Advent2022
+  module Day01
+    class << self
+      def hello
+        puts 'hello'
+      end
+    end
+  end
+end
