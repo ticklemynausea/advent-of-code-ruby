b0c38522c5ae625b213bcfa12ef5df3e98fdc3bb
Mário Carneiro <emailoftheyear@gmail.com>
2022-12-01 15:15:26 +0000

    Add nice boilerplate

--- lib/advent2022/day01/day01.rb
+++ lib/advent2022/day01/day01.rb
@@ -1,27 +1,25 @@
-module Advent2022
-  module Day01
-    class << self
-      def elves_total
-        elves = [0]
+# frozen_string_literal: true
 
-        STDIN.each do |line|
-          if line.chomp.empty?
-            elves << 0
-          else
-            elves.push(elves.pop + line.to_i)
-          end
-        end
+class Advent2022::Day01
+  def elves_total
+    elves = [0]
 
-        elves
-      end
-
-      def problem1
-        puts elves_total.max
-      end
-
-      def problem2
-        puts elves_total.sort.last(3).inject(&:+)
+    $stdin.each do |line|
+      if line.chomp.empty?
+        elves << 0
+      else
+        elves.push(elves.pop + line.to_i)
       end
     end
+
+    elves
+  end
+
+  def problem1
+    puts elves_total.max
+  end
+
+  def problem2
+    puts elves_total.sort.last(3).inject(&:+)
   end
 end
