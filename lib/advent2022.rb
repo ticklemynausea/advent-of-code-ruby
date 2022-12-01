6348e5475f5bf32eaab79ca4dc1982dffdb79cfd
Mário Carneiro <emailoftheyear@gmail.com>
2022-12-01 14:59:15 +0000

    Problem 1 + some boilerplate

--- lib/advent2022.rb
+++ lib/advent2022.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "advent2022/version"
-require_relative "advent2022/day01"
+require_relative "advent2022/day01/day01"
 
 module Advent2022
   class Error < StandardError; end
--- lib/advent2022/day01/day01.rb
+++ lib/advent2022/day01/day01.rb
@@ -1,8 +1,18 @@
 module Advent2022
   module Day01
     class << self
-      def hello
-        puts 'hello'
+      def problem1
+        elves = [0]
+
+        STDIN.each do |line|
+          if line.chomp.empty?
+            elves << 0
+          else
+            elves.push(elves.pop + line.to_i)
+          end
+        end
+
+        puts elves.max
       end
     end
   end
