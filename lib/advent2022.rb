b3956a52994e92d5982668db5a45c021800b3743
Mário Carneiro <emailoftheyear@gmail.com>
2022-12-02 10:56:33 +0000

    Day 2

--- lib/advent2022.rb
+++ lib/advent2022.rb
@@ -2,6 +2,7 @@
 
 require_relative "advent2022/version"
 require_relative "advent2022/day01/day01"
+require_relative "advent2022/day02/day02"
 
 module Advent2022
   class Error < StandardError; end
---
+++ lib/advent2022/day02/day02.rb
@@ -0,0 +1,38 @@
+class Advent2022::Day02
+  def shape_score(me)
+    { "X" => 1, "Y" => 2, "Z" => 3 }.fetch(me)
+  end
+
+  def play_score(op, me)
+    return 0 if ["A Z", "B X", "C Y"].find { |s| s == "#{op} #{me}" }
+    return 3 if ["A X", "B Y", "C Z"].find { |s| s == "#{op} #{me}" }
+
+    6
+  end
+
+  def desired_play(op, res)
+    return "X" if ["A Y", "B X", "C Z"].find { |s| s == "#{op} #{res}" }
+    return "Y" if ["A Z", "B Y", "C X"].find { |s| s == "#{op} #{res}" }
+
+    "Z"
+  end
+
+  def problem1
+    totals = $stdin.map do |line|
+      op, me = line.split
+      shape_score(me) + play_score(op, me)
+    end
+
+    puts totals.inject(&:+)
+  end
+
+  def problem2
+    totals = $stdin.map do |line|
+      op, res = line.split
+      me = desired_play(op, res)
+      shape_score(me) + play_score(op, me)
+    end
+
+    puts totals.inject(&:+)
+  end
+end
