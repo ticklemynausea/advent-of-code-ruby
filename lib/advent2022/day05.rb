d69445e2292a6c816b2dbfa419e5a7ade974f15f
Mário Carneiro <emailoftheyear@gmail.com>
2022-12-05 10:41:06 +0000

    Day 5 Pt 2

--- lib/advent2022/day05.rb
+++ lib/advent2022/day05.rb
@@ -4,18 +4,21 @@ class Advent2022::Day05
   def problem1
     load_problem
 
-    $stdin.each_line do |line|
-      amount, from, to = line.scan(/(\d+)/).map { |r| r.first.to_i }
-      puts "#{amount} #{from} #{to}"
-      amount.times do
-        stacks[to - 1].push(stacks[from - 1].pop)
-      end
+    each_command do |amount, from, to|
+      stacks[to - 1].push(*stacks[from - 1].pop(amount).reverse)
     end
 
     stacks.map(&:last).join
   end
 
   def problem2
+    load_problem
+
+    each_command do |amount, from, to|
+      stacks[to - 1].push(*stacks[from - 1].pop(amount))
+    end
+
+    stacks.map(&:last).join
   end
 
   private
@@ -36,6 +39,13 @@ class Advent2022::Day05
     end
 
     @stacks = rows.transpose.map { |row| row.filter(&:itself).reverse }
+  end
+
+  def each_command
+    $stdin.each_line do |line|
+      amount, from, to = line.scan(/(\d+)/).map { |r| r.first.to_i }
+      yield(amount, from, to)
+    end
   end
 
   attr_reader :stacks
