bccb7cfccefdb0984101c7936a68691da08a2f24
Mário Carneiro <emailoftheyear@gmail.com>
2022-12-21 00:54:08 +0000

    Refactor day 20

--- lib/advent_of_code/year_2022/day20.rb
+++ lib/advent_of_code/year_2022/day20.rb
@@ -2,44 +2,45 @@ class AdventOfCode::Year2022::Day20
   DECRYPTION_KEY = 811589153
 
   def problem1
-    original_list = list.dup
+    mix(list)
 
-    original_list.each do |element|
-      position = list.index(element)
-      number = element.first
-
-      new_position = (number + position) % (list.length - 1)
-      list.delete_at(position)
-      list.insert(new_position, element)
-    end
-
-    zero_at = list.find_index { |element| element.first == 0 }
     [1000, 2000, 3000]
-      .map { |p| list[(zero_at + p) % original_list.length] }
+      .map { |n| after_zero(list, n) }
       .map(&:first)
       .reduce(&:+)
   end
 
   def problem2
-    keyed_list = list.map { |element| [element.first * DECRYPTION_KEY, element.last] }
-    original_list = keyed_list.dup
-
-    10.times do
-      original_list.each do |element|
-        position = keyed_list.index(element)
-        number = element.first
-
-        new_position = (number + position) % (keyed_list.length - 1)
-        keyed_list.delete_at(position)
-        keyed_list.insert(new_position, element)
-      end
+    keyed_list = list.map do |element|
+      [element.first * DECRYPTION_KEY, element.last]
     end
 
-    zero_at = keyed_list.find_index { |element| element.first == 0 }
+    mix(keyed_list, 10)
+
     [1000, 2000, 3000]
-      .map { |p| keyed_list[(zero_at + p) % original_list.length] }
+      .map { |n| after_zero(keyed_list, n) }
       .map(&:first)
       .reduce(&:+)
+  end
+
+  def mix(list, times = 1)
+    original_list = list.dup
+
+    times.times do
+      original_list.each do |element|
+        position = list.index(element)
+        number = element.first
+
+        new_position = (number + position) % (list.length - 1)
+        list.delete_at(position)
+        list.insert(new_position, element)
+      end
+    end
+  end
+
+  def after_zero(list, n)
+    zero_at = list.find_index { |element| element.first == 0 }
+    list[(zero_at + n) % list.length]
   end
 
   def list
--- spec/lib/advent_of_code/year_2022/day20_spec.rb
+++ spec/lib/advent_of_code/year_2022/day20_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe AdventOfCode::Year2022::Day20 do
 
     it_behaves_like "daily problem" do
       let(:problem1_solution) { 3 }
-      let(:problem2_solution) { 18257 }
+      let(:problem2_solution) { 1623178306 }
     end
   end
 
   it_behaves_like "daily problem" do
-    let(:problem1_solution) { nil }
-    let(:problem2_solution) { nil }
+    let(:problem1_solution) { 18257 }
+    let(:problem2_solution) { 4148032160983 }
   end
 end
